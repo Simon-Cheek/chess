d00902e3941250339fe90d4ebdaa6d4fb1865ae4
Simon Cheek <simon.cheek@yahoo.com>
2024-10-28 16:09:41 -0600

    Finish Auth Unit Tests

--- server/src/test/java/dataaccess/DAOTests.java
+++ server/src/test/java/dataaccess/DAOTests.java
@@ -1,6 +1,7 @@
 package dataaccess;
 
 import exception.ResponseException;
+import model.AuthRecord;
 import model.UserRecord;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,4 +35,48 @@ public class DAOTests {
         Service service2 = new Service();
         assertThrows(ResponseException.class, () -> service2.registerUser(invalidUser));
     }
+    @Test
+    @DisplayName("Auth - Retrieves Auth")
+    public void testGetAuth() throws ResponseException {
+        DBAuthDAO authDAO = new DBAuthDAO();
+        AuthRecord record = authDAO.createAuth("testUser1");
+        assertDoesNotThrow(() -> authDAO.getAuthByToken(record.authToken()));
+    }
+
+    @Test
+    @DisplayName("Auth - Doesn't Retrieves Auth with Invalid Data")
+    public void testGetInvalidAuth() throws ResponseException {
+        DBAuthDAO authDAO = new DBAuthDAO();
+        assertNull(authDAO.getAuthByToken("randomTokenNotValid"));
+    }
+
+    @Test
+    @DisplayName("Auth - Delete Auth with Token")
+    public void testDeleteAuth() throws ResponseException {
+        DBAuthDAO authDAO = new DBAuthDAO();
+        AuthRecord auth = authDAO.createAuth("testUser3");
+        assertDoesNotThrow(() -> authDAO.deleteAuthByToken(auth.authToken()));
+    }
+
+    @Test
+    @DisplayName("Auth - Does Not Delete Auth with Invalid Token")
+    public void testDeleteInvalidAuth() throws ResponseException {
+        Service service = new Service();
+        assertThrows(ResponseException.class, () -> service.logoutUser("invalidTokenHaha"));
+    }
+
+    @Test
+    @DisplayName("Auth - Deletes All Auths")
+    public void testDeleteAllAuth() throws ResponseException {
+
+        DBAuthDAO authDAO1 = new DBAuthDAO();
+        AuthRecord record = authDAO1.createAuth("newUser");
+
+        DBAuthDAO authDAO2 = new DBAuthDAO();
+        assertDoesNotThrow(authDAO2::deleteAuths);
+        assertNull(authDAO1.getAuthByToken(record.authToken()));
+    }
+
+
+
 }
