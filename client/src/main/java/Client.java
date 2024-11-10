79b6ea347b63dded378484e4ccc209182eb2a529
Simon Cheek <simon.cheek@yahoo.com>
2024-11-09 19:31:26 -0700

    Logged in help

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -73,7 +73,7 @@ public class Client {
 
     public String help() {
 
-        return String.format("""
+        String notLoggedin = String.format("""
                 %s register <USERNAME> <PASSWORD> <EMAIL> %s - to create an account
                 %s login <USERNAME> <PASSWORD> %s - to play chess
                 %s quit %s - playing chess
@@ -87,6 +87,32 @@ public class Client {
                 EscapeSequences.SET_TEXT_COLOR_BLUE,
                 EscapeSequences.RESET_TEXT_COLOR
         );
+
+        String loggedIn = String.format("""
+                %s create <NAME> %s - a game
+                %s list %s - games
+                %s join <ID> [WHITE|BLACK] %s - a game
+                %s observe <ID> %s - a game
+                %s logout %s - when you are done
+                %s quit %s - playing chess
+                %s help %s - with possible commands
+                """, EscapeSequences.SET_TEXT_COLOR_BLUE,
+                EscapeSequences.RESET_TEXT_COLOR,
+                EscapeSequences.SET_TEXT_COLOR_BLUE,
+                EscapeSequences.RESET_TEXT_COLOR,
+                EscapeSequences.SET_TEXT_COLOR_BLUE,
+                EscapeSequences.RESET_TEXT_COLOR,
+                EscapeSequences.SET_TEXT_COLOR_BLUE,
+                EscapeSequences.RESET_TEXT_COLOR,
+                EscapeSequences.SET_TEXT_COLOR_BLUE,
+                EscapeSequences.RESET_TEXT_COLOR,
+                EscapeSequences.SET_TEXT_COLOR_BLUE,
+                EscapeSequences.RESET_TEXT_COLOR,
+                EscapeSequences.SET_TEXT_COLOR_BLUE,
+                EscapeSequences.RESET_TEXT_COLOR
+        );
+
+        return this.isLoggedIn() ? loggedIn : notLoggedin;
     }
 
     public boolean isLoggedIn() {
